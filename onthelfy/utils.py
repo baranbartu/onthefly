7f7fb3a66048aa95cb9c6fcc2ec3d3c95e059778
baranbartu <bbartu.demirci@gmail.com>
2016-12-21 10:47:18 +0200

    fixed load_class method

--- onthelfy/utils.py
+++ onthelfy/utils.py
@@ -1,6 +1,27 @@
-def load_class(name):
-    components = name.split('.')
-    mod = __import__(components[0])
-    for comp in components[1:]:
-        mod = getattr(mod, comp)
-    return mod
+import os
+import imp
+
+def load_class(path):
+    """
+    Loads class from path.
+    """
+
+    mod_name, klass_name = path.rsplit('.', 1)
+    mod_args = mod_name.split('.')
+    mod_args.pop(0)
+
+    try:
+        import onthelfy as djonthefly
+        mod_path = '%s/%s/%s.py' % (
+            os.path.dirname(djonthefly.__file__), mod_args[0], mod_args[1])
+        mod = imp.load_source(mod_name, mod_path)
+    except AttributeError as e:
+        raise ImproperlyConfigured('Error importing {0}: "{1}"'.format(mod_name, e))
+
+    try:
+        klass = getattr(mod, klass_name)
+    except AttributeError:
+        raise ImproperlyConfigured('Module "{0}" does not define a "{1}" class'.format(mod_name, klass_name))
+
+    return klass
+
